6cfb695431581dfb33eed5b3a1813647171f99cc
Henry Precheur <henry@precheur.org>
2022-07-20 12:56:32 -0700

    Clean-ups

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,12 +10,12 @@ then
     exit 1
 fi
 
-kubectl apply -f rbac-kube-system.yaml
+kubectl --namespace kube-system apply -f rbac-kube-system.yaml
+
 kubectl_apply() {
     kubectl --namespace "$namespace" apply -f "$*"
 }
-sed "s/##NAMESPACE##/$namespace/" < rbac.yaml | tee /dev/stderr | kubectl_apply -
-# kubectl_apply rbac.yaml
+sed "s/##NAMESPACE##/$namespace/" < rbac.yaml | kubectl_apply -
 kubectl_apply service.yaml
 kubectl_apply buildkite-deployment.yaml
 sed "s/##NAMESPACE##/$namespace/" < apiservice.yaml | kubectl_apply -
